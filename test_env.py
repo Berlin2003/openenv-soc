9d9dbb55e079d6c55682bf9d557af93e33956160
christoberlin <christoberlin@christoberlins-MacBook-Air.local>
2026-03-30 22:16:19 +0530

    deleted test

--- test_env.py
+++
@@ -1,8 +0,0 @@
-from server.environment import SOCEnvironment
-from models import SOCAction
-
-env = SOCEnvironment()
-obs = env.reset(task="easy")
-act = SOCAction(action_type="query_threat_intel", indicator="login.secure-oauth.com")
-obs2 = env.step(act)
-print("SUCCESS. obs2 reward:", getattr(obs2, 'reward', None))
